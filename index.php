20742d03bc06f927069bed6a293f1b1345ec889d
Guillaume Perbet <perbet.dev@gmail.com>
2020-06-15 14:41:07 +0200

    home page

--- index.php
+++ index.php
@@ -6,6 +6,19 @@
     <title>Explorateur de fichiers</title>
 </head>
 <body>
-    
+    <?php
+    require_once("print-items.php");
+
+    // print content of home folder
+    chdir(getcwd() . DIRECTORY_SEPARATOR . "home");
+    $content = scandir(getcwd());
+    $items = [];
+    foreach($content as $item ){
+        if($item !== "." && $item !== ".."){
+            array_push($items, ["name"=>$item, "isFolder"=>is_dir($item)]);
+        }
+    }
+    printItems(json_encode($items));
+    ?>
 </body>
 </html>
