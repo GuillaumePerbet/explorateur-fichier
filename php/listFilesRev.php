aa396e06800a11e1a7e681dbaf1079afff07a969
n3wborn <n3wborn@protonmail.com>
2020-06-16 15:33:21 +0200

    add listFilesRev.php script

---
+++ php/listFilesRev.php
@@ -0,0 +1,33 @@
+<?php
+
+// get content of targeted directory
+$url = $_POST["url"];
+chdir($url);
+// hide useless entries of $content
+$content = array_diff(scandir(getcwd()), array('..', '.'));
+// reverse order
+$contentRev = array_reverse($content);
+$response = [];
+
+foreach($contentRev as $item ){
+
+	// check if item is file or folder
+	if (is_dir($item)){
+		$type = "folder";
+		$event = "navigate(".json_encode($url . DIRECTORY_SEPARATOR . $item).")";
+    }else{
+		$type = "file";
+		$event = "";
+    }
+
+	// add item to $response
+	array_push($response,
+	"<figure class='item' onclick=$event>
+		<img src=media/$type.png alt=$type>
+		<figcaption>$item</figcaption>
+	</figure>"
+	);
+
+}
+
+echo json_encode($response);
