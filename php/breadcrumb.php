4b15d5923271959be7ccc2c366f2ac7c4a2e2e07
Guillaume Perbet <perbet.dev@gmail.com>
2020-06-16 13:18:58 +0200

    add navigation to breadcrumb

--- php/breadcrumb.php
+++ php/breadcrumb.php
@@ -3,14 +3,23 @@
 $path = explode(DIRECTORY_SEPARATOR,$_POST["url"]);
 $print = false;
 $response=[];
+$url = "";
 foreach($path as $item){
+    $url = $url . $item;
     // start printing at "home" directory
     if($item == "home"){
         $print = true;
     }
     if($print){
-        array_push($response,"<li><a href='#'>$item</a></li>");
+        array_push($response,
+            "<li>
+                <a onclick='navigate(".json_encode($url).")'>
+                    $item
+                </a>
+            </li>"
+        );
     }
+    $url = $url . DIRECTORY_SEPARATOR;
 }
 
 echo json_encode($response);
