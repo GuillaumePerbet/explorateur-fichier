080960162d695c269bad584a4a94e3038ff75e12
n3wborn <n3wborn@protonmail.com>
2020-06-16 14:59:46 +0200

    rewrite hiding dot folders

--- php/listFiles.php
+++ php/listFiles.php
@@ -3,31 +3,29 @@
 // get content of targeted directory
 $url = $_POST["url"];
 chdir($url);
-$content = scandir(getcwd());
+// hide useless entries of $content
+$content = array_diff(scandir(getcwd()), array('..', '.'));;
 $response = [];
 
 foreach($content as $item ){
 
-	// hide useless entries of $content
-	if($item !== "." && $item !== ".."){
+	// check if item is file or folder
+	if (is_dir($item)){
+		$type = "folder";
+		$event = "navigate(".json_encode($url . DIRECTORY_SEPARATOR . $item).")";
+    }else{
+		$type = "file";
+		$event = "";
+    }
 
-		// check if item is file or folder
-		if (is_dir($item)){
-			$type = "folder";
-			$event = "navigate(".json_encode($url . DIRECTORY_SEPARATOR . $item).")";
-        }else{
-			$type = "file";
-			$event = "";
-        }
+	// add item to $response
+	array_push($response,
+	"<figure class='item' onclick=$event>
+		<img src=media/$type.png alt=$type>
+		<figcaption>$item</figcaption>
+	</figure>"
+	);
 
-		// add item to $response
-		array_push($response,
-		"<figure class='item' onclick=$event>
-			<img src=media/$type.png alt=$type>
-			<figcaption>$item</figcaption>
-		</figure>"
-		);
-	}
 }
 
 echo json_encode($response);
