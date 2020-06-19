8720cacefee68eee51bda3febfa2302e3bfa4a5a
Guillaume Perbet <perbet.dev@gmail.com>
2020-06-19 08:24:39 +0200

    add style to buttons

--- index.php
+++ index.php
@@ -18,16 +18,16 @@
         <section id="createFiles" class="container">
             <form id="createFolder" action="">
                 <input type="text" name="folderName" value="Nouveau dossier">
-                <input type="submit" value="Créer un dossier">
+                <input class="button" type="submit" value="Créer un dossier">
             </form>
             <form id="createFile" action="">
                 <input type="text" name="fileName" value="Nouveau fichier">
-                <input type="submit" value="Créer un fichier">
+                <input class="button" type="submit" value="Créer un fichier">
             </form>
-            <button onclick="pasteFile()">Coller ici</button>
+            <button class="button" onclick="pasteFile()">Coller ici</button>
         </section>
         <section id="sortSection"  class="container">
-            <button id="sortBtn">Tri par Nom</button>
+            <button class="button" id="sortBtn">Tri par Nom</button>
         </section>
         <section id="listFiles" class="container"></section>
 
